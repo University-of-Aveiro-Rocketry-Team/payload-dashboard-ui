e78ad3b4525e66af291fac42e04077e1d6c5a3be
andreclerigo <andreclerigo@outlook.com>
2023-12-07 10:55:51 +0000

    changing .build to .prod

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-docker build -t andreclerigo/payload-dashboard-ui:latest-arm64-build -f ../Dockerfile.build ..
-docker push andreclerigo/payload-dashboard-ui:latest-arm64-build
 docker compose pull ui
 docker compose up -d --no-deps --force-recreate ui
